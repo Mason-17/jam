057a027bebdb1a62e16aa3ea374ad405b28445e8
Mason <122631948+Mason-17@users.noreply.github.com>
2025-07-24 12:48:17 -0700

    restarting :(

--- src/assets/pages/albums.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-class Albums:
-    def __init__(self, page, Globals):
-        self.page = page
-        self.Globals = Globals
-
-    def gen_page(self):
-        pass
--- src/main.py
+++ src/main.py
@@ -1,17 +1,2 @@
 import flet as ft
-from assets import navi as navi
-from assets import globals as Globals
-from assets.pages.home import Home
-from assets.pages.page2 import Page2
-from assets.pages.page3 import Page3
 
-#Globals = globals.Globals()
-page_view_history = ["/","/2","/3"]
-app_page_classes = []
-
-def main(page: ft.Page):
-    page.add(Home.gen_gallery)
-    #page.update()
-
-
-ft.app(target=main)
