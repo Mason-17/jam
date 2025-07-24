fce7bf0e51a76603b26e1cfb123cdc4bb8271a7c
Mason <122631948+Mason-17@users.noreply.github.com>
2025-07-24 13:40:28 -0700

    Added album destination

--- src/assets/nav_bar.py
+++ src/assets/nav_bar.py
@@ -3,6 +3,7 @@ import flet as ft
 def nav_bar():
     return ft.NavigationBar(
         destinations=[
-            ft.NavigationBarDestination(icon=ft.Icons.MUSIC_NOTE,label="TRACKS")
+            ft.NavigationBarDestination(icon=ft.Icons.MUSIC_NOTE,label="TRACKS"),
+            ft.NavigationBarDestination(icon=ft.Icons.ALBUM,label="ALBUMS")
         ]
     )
