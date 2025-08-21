f7820a5fd79824544879d4964902b14845b1934c
Mason <122631948+Mason-17@users.noreply.github.com>
2025-08-21 12:34:32 -0700

    minor stuff

--- src/assets/globals.py
+++
@@ -1,6 +0,0 @@
-
-
-class Globals:
-    def __init__(self):
-        self.sample_var = 10
-        self.title = "TITLE"
--- src/controls.py
+++ src/controls.py
@@ -9,9 +9,10 @@ class Controls:
     player_bar = ft.BottomAppBar(
         #shape=None,
         content=ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
             controls=[
-                ft.IconButton(icon=ft.Icons.PLAY_ARROW,),
                 ft.IconButton(icon=ft.Icons.SKIP_PREVIOUS),
+                ft.IconButton(icon=ft.Icons.PLAY_ARROW,),
                 ft.IconButton(icon=ft.Icons.SKIP_NEXT),
             ]
         )
